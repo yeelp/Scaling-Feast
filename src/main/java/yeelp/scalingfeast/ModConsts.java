6bb4a845d07de18c83030ac69c7509c8d02ec2c4
Yeelp <eliteluigi38@gmail.com>
2020-03-07 17:16:14 -0500

    Added useful mod constants

---
+++ src/main/java/yeelp/scalingfeast/ModConsts.java
@@ -0,0 +1,9 @@
+package yeelp.scalingfeast;
+
+public class ModConsts 
+{
+	public static final String MOD_ID = "scalingfeast";
+	public static final String MOD_NAME = "Scaling Feast";
+	public static final String MOD_VERSION = "1.0";
+	public static final String MC_VERSION = "1.12.2";
+}
