8aaee3c525b91afa9cd277d6f4af33bc72251aeb
Yeelp <eliteluigi38@gmail.com>
2020-03-23 20:45:42 -0400

    Began Loot injector

---
+++ src/main/java/yeelp/scalingfeast/handlers/LootTableInjector.java
@@ -0,0 +1,13 @@
+package yeelp.scalingfeast.handlers;
+
+import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class LootTableInjector extends Handler 
+{
+	@SubscribeEvent
+	public void onLoot(LootTableLoadEvent evt)
+	{
+		
+	}
+}
