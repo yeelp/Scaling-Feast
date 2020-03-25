e66d7ffe6b9f38902b45b5edfe6210035a5a8303
Yeelp <eliteluigi38@gmail.com>
2020-03-25 19:56:56 -0400

    Update versioning for pre-release

--- src/main/java/yeelp/scalingfeast/ModConsts.java
+++ src/main/java/yeelp/scalingfeast/ModConsts.java
@@ -4,12 +4,11 @@ public class ModConsts
 {
 	public static final String MOD_ID = "scalingfeast";
 	public static final String MOD_NAME = "Scaling Feast";
-	public static final String MOD_VERSION = "1.0";
+	public static final String MOD_VERSION = "v1.0.0-alpha";
 	public static final String MC_VERSION = "1.12.2";
 	public static final int VANILLA_MAX_HUNGER = 20;
 	public static final float VANILLA_MAX_SAT = 20.0f;
 	
-	public static final String GUI_FACTORY = "yeelp.scalingfeast.config.SFConfigGuiFactory";
 	public static final String CLIENT_PROXY = "yeelp.scalingfeast.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "yeelp.scalingfeast.proxy.ServerProxy";
 }
