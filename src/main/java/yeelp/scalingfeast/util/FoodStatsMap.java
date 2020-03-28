70d7824909563cdaf17df1c66b252a9733277ac0
Yeelp <eliteluigi38@gmail.com>
2020-03-27 20:31:54 -0400

    Deprecating FoodStatsMap

--- src/main/java/yeelp/scalingfeast/util/FoodStatsMap.java
+++ src/main/java/yeelp/scalingfeast/util/FoodStatsMap.java
@@ -11,6 +11,7 @@ import java.util.UUID;
  * @author Yeelp
  * @see ExtendedFoodStats
  * @see <a href="https://minecraft.gamepedia.com/Hunger#Mechanics">Minecraft's basic hunger mechanics</a>
+ * @deprecated No longer needed
  */
 public class FoodStatsMap 
 {
