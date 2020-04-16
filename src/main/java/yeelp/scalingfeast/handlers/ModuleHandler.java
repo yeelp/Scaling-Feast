e229316341b80b245ebc00cc6975ec1f7b2999be
Yeelp <eliteluigi38@gmail.com>
2020-04-15 22:53:55 -0400

    Fixed a crash on servers with client side events

--- src/main/java/yeelp/scalingfeast/handlers/ModuleHandler.java
+++ src/main/java/yeelp/scalingfeast/handlers/ModuleHandler.java
@@ -23,6 +23,7 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import squeek.applecore.api.AppleCoreAPI;
 import squeek.applecore.api.food.FoodEvent;
 import yeelp.scalingfeast.ModConfig;
@@ -119,6 +120,7 @@ public class ModuleHandler extends Handler
 	}
 	
 	@SubscribeEvent
+	@SideOnly(value = Side.CLIENT)
 	public void onTooltip(ItemTooltipEvent evt)
 	{
 		if(SpiceOfLifeHelper.isEnabled() && evt.getEntityPlayer() != null && evt.getItemStack() != null && AppleCoreAPI.accessor.isFood(evt.getItemStack()))
